c2691f39497fbb590e86e2eb5100527dc5ed277b
Casper <casper.eide@gmail.com>
2024-03-21 12:41:28 +0100

    v3     - Added camera rotation     - Added camera pitch up/down

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VoxelSpaceEngine
 
-A voxel space engine made using [dos-like](https://github.com/mattiasgustavsson/dos-like) and the C programming language by following the YouTube video [Voxel Space (Comanche Terrain Rendering)](https://youtu.be/bQBY9BM9g_Y) by Gustavo Pezzi.
+Voxel space engine made using the [DOS-like](https://github.com/mattiasgustavsson/dos-like) framework with the C programming language by following the YouTube video [Voxel Space (Comanche Terrain Rendering)](https://youtu.be/bQBY9BM9g_Y) by Gustavo Pezzi.
 
-The render algorithm of the Voxel Space Engine is also explained by Sebastian Macke [here](https://github.com/s-macke/VoxelSpace).
+The render algorithm is also explained by Sebastian Macke [here](https://github.com/s-macke/VoxelSpace).
 
 Original source: https://github.com/gustavopezzi/voxelspace.git
--- main.c
+++ main.c
@@ -6,43 +6,67 @@
 
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 200
-#define SCALE_FACTOR 100.0
+#define MAP_N 1024
+#define SCALE_FACTOR 70.0
 
 uint8_t* heightmap = NULL;
 uint8_t* colormap = NULL;
 
 typedef struct {
-    float x;
-    float y;
-    float height;
-    float zfar;
+    float x;       // x position on the map
+    float y;       // y position on the map
+    float height;  // height from sea level
+    float angle;   // clockwise horizontal rotation angle in radians
+    float horizon; // pitch up/down
+    //float tilt;    // tilt/lean left right
+    float zfar;    // max render distance
 } camera_t;
 
 camera_t camera = {
-    .x      = 512,
-    .y      = 512,
-    .height = 100,
-    .zfar   = 400
+    .x       = 512.0,
+    .y       = 512.0,
+    .height  = 100.0,
+    .angle   = 1.5 * 3.141592,
+    .horizon = 75.0,
+    //.tilt    = 0.0,
+    .zfar    = 1000.0
 };
 
 void processinput() {
-    if (keystate(KEY_UP)) {
-        camera.y++;
+    if (keystate(KEY_W)) {
+        camera.x += cos(camera.angle);
+        camera.y += sin(camera.angle);
     }
-    if (keystate(KEY_DOWN)) {
-        camera.y--;
+    if (keystate(KEY_S)) {
+        camera.x -= cos(camera.angle);
+        camera.y -= sin(camera.angle);
     }
-    if (keystate(KEY_LEFT)) {
-        camera.x--;
+    if (keystate(KEY_A)) {
+        camera.angle -= 0.01;
+        //camera.tilt = 0.04;
+
     }
-    if (keystate(KEY_RIGHT)) {
-        camera.x++;
+    if (keystate(KEY_D)) {
+        camera.angle += 0.01;
+        //camera.tilt = -0.04;
     }
+    
+    //float min_tilt = -0.5;
+    //float max_tilt = 0.5;
+    //float tilt_factor = 0.5;
+    //camera.tilt = min_tilt + (tilt_factor * (max_tilt - min_tilt));
+
     if (keystate(KEY_E)) {
         camera.height++;
     }
-    if (keystate(KEY_D)) {
+    if (keystate(KEY_Q)) {
         camera.height--;
+    }
+    if (keystate(KEY_UP)) {
+        camera.horizon += 1.5;
+    }
+    if (keystate(KEY_DOWN)) {
+        camera.horizon -= 1.5;
     }
 }
 
@@ -50,17 +74,17 @@ int main(int argc, char* args[]) {
     setvideomode(videomode_320x200);
 
     uint8_t palette[256 * 3];
-    int map_width, map_height, pal_count;
+    int pal_count;
 
-    colormap = loadgif("maps/gif/map3.color.gif", &map_width, &map_height, &pal_count, palette);
-    heightmap  = loadgif("maps/gif/map3.height.gif", &map_width, &map_height, NULL, NULL);
+    colormap = loadgif("maps/gif/map3.color.gif", NULL, NULL, &pal_count, palette);
+    heightmap  = loadgif("maps/gif/map3.height.gif", NULL, NULL, NULL, NULL);
 
 
     for (int i = 0; i < pal_count; i++) {
         setpal(i, palette[3 * i + 0], palette[3 * i + 1], palette[3 * i + 2]);
     }
 
-    setpal(0, 36, 36, 56);
+    //setpal(0, 36, 36, 56);
     
     setdoublebuffer(1);
 
@@ -69,14 +93,16 @@ int main(int argc, char* args[]) {
     while (!shuttingdown()) {
         waitvbl();
         clearscreen();
-
         processinput();
 
-        float plx = -camera.zfar;
-        float ply = +camera.zfar;
+        float sinangle = sin(camera.angle);
+        float cosangle = cos(camera.angle);
 
-        float prx = +camera.zfar;
-        float pry = +camera.zfar;
+        float plx = cosangle * camera.zfar + sinangle * camera.zfar;
+        float ply = sinangle * camera.zfar - cosangle * camera.zfar;
+
+        float prx = cosangle * camera.zfar - sinangle * camera.zfar;
+        float pry = sinangle * camera.zfar + cosangle * camera.zfar;
 
         // Loop through the 320 rays from left to right
         for (int i = 0; i < SCREEN_WIDTH; i++) {
@@ -96,24 +122,28 @@ int main(int argc, char* args[]) {
                 // Convert position to map position
                 int mapoffset = ((1024 * ((int)(ry) & 1023)) + ((int)(rx) & 1023));
 
-                int heightonscreen = (int)((camera.height - heightmap[mapoffset]) / z * SCALE_FACTOR);
+                int proj_height = (int)((camera.height - heightmap[mapoffset]) / z * SCALE_FACTOR + camera.horizon);
 
-                if (heightonscreen < 0) {
-                    heightonscreen = 0;
+                if (proj_height < 0) {
+                    proj_height = 0;
                 }
 
-                if (heightonscreen > SCREEN_HEIGHT) {
-                    heightonscreen = SCREEN_HEIGHT - 1;
+                if (proj_height > SCREEN_HEIGHT) {
+                    proj_height = SCREEN_HEIGHT - 1;
                 }
 
                 // Only render pixels if height is higher than previously rendered terrain at current x position
-                if (heightonscreen < max_height) {
-                    for (int y = heightonscreen; y < max_height; y++) {
+                if (proj_height < max_height) {
+
+                    // tilt factor "lean" from -1 to +1
+                    //float lean = (camera.tilt * (i / (float)SCREEN_WIDTH - 0.5) + 0.5) * SCREEN_HEIGHT / 6;
+                    //for (int y = (proj_height + lean); y < (max_height + lean); y++) {
+                    for (int y = proj_height; y < max_height; y++) {
                         if (y >= 0) {
                             framebuffer[(SCREEN_WIDTH * y) + i] = (uint8_t)colormap[mapoffset];
                         }
                     }
-                    max_height = heightonscreen;
+                    max_height = proj_height;
                 }
             }
 
