8730ff706a30a768907f50c499fa943813ce7d3e
Casper <casper.eide@gmail.com>
2024-03-21 11:04:46 +0100

    v2     - Added camera_t camera type and a camera object     - Looping throught the currently selected map image and project pixels from the map on screen     - Added keyboard controls for the camera     - Added looping of the map when reaching boundries

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # VoxelSpaceEngine
 
-A voxel space engine made using the C programming language by following the YouTube video [Voxel Space (Comanche Terrain Rendering)](https://youtu.be/bQBY9BM9g_Y) by Gustavo Pezzi.
+A voxel space engine made using [dos-like](https://github.com/mattiasgustavsson/dos-like) and the C programming language by following the YouTube video [Voxel Space (Comanche Terrain Rendering)](https://youtu.be/bQBY9BM9g_Y) by Gustavo Pezzi.
+
+The render algorithm of the Voxel Space Engine is also explained by Sebastian Macke [here](https://github.com/s-macke/VoxelSpace).
 
 Original source: https://github.com/gustavopezzi/voxelspace.git
--- main.c
+++ main.c
@@ -6,17 +6,59 @@
 
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 200
+#define SCALE_FACTOR 100.0
 
 uint8_t* heightmap = NULL;
 uint8_t* colormap = NULL;
+
+typedef struct {
+    float x;
+    float y;
+    float height;
+    float zfar;
+} camera_t;
+
+camera_t camera = {
+    .x      = 512,
+    .y      = 512,
+    .height = 100,
+    .zfar   = 400
+};
+
+void processinput() {
+    if (keystate(KEY_UP)) {
+        camera.y++;
+    }
+    if (keystate(KEY_DOWN)) {
+        camera.y--;
+    }
+    if (keystate(KEY_LEFT)) {
+        camera.x--;
+    }
+    if (keystate(KEY_RIGHT)) {
+        camera.x++;
+    }
+    if (keystate(KEY_E)) {
+        camera.height++;
+    }
+    if (keystate(KEY_D)) {
+        camera.height--;
+    }
+}
 
 int main(int argc, char* args[]) {
     setvideomode(videomode_320x200);
 
     uint8_t palette[256 * 3];
     int map_width, map_height, pal_count;
-    colormap = loadgif("files/colormap.gif", &map_width, &map_height, &pal_count, palette);
-    heightmap = loadgif("files/heightmap.gif", &map_width, &map_height, NULL, NULL);
+
+    colormap = loadgif("maps/gif/map3.color.gif", &map_width, &map_height, &pal_count, palette);
+    heightmap  = loadgif("maps/gif/map3.height.gif", &map_width, &map_height, NULL, NULL);
+
+
+    for (int i = 0; i < pal_count; i++) {
+        setpal(i, palette[3 * i + 0], palette[3 * i + 1], palette[3 * i + 2]);
+    }
 
     setpal(0, 36, 36, 56);
     
@@ -28,10 +70,54 @@ int main(int argc, char* args[]) {
         waitvbl();
         clearscreen();
 
-        int x = 160;
-        int y = 100;
+        processinput();
 
-        framebuffer[(SCREEN_WIDTH * y) + x] = 0x50;
+        float plx = -camera.zfar;
+        float ply = +camera.zfar;
+
+        float prx = +camera.zfar;
+        float pry = +camera.zfar;
+
+        // Loop through the 320 rays from left to right
+        for (int i = 0; i < SCREEN_WIDTH; i++) {
+            float delta_x = (plx + (prx - plx) / SCREEN_WIDTH * i) / camera.zfar;
+            float delta_y = (ply + (pry - ply) / SCREEN_WIDTH * i) / camera.zfar;
+
+            float rx = camera.x;
+            float ry = camera.y;
+
+            float max_height = SCREEN_HEIGHT;
+
+            // Loop through the positions of the ray from camera to end-point on zfar
+            for (int z = 1; z < camera.zfar; z++) {
+                rx += delta_x;
+                ry += delta_y;
+
+                // Convert position to map position
+                int mapoffset = ((1024 * ((int)(ry) & 1023)) + ((int)(rx) & 1023));
+
+                int heightonscreen = (int)((camera.height - heightmap[mapoffset]) / z * SCALE_FACTOR);
+
+                if (heightonscreen < 0) {
+                    heightonscreen = 0;
+                }
+
+                if (heightonscreen > SCREEN_HEIGHT) {
+                    heightonscreen = SCREEN_HEIGHT - 1;
+                }
+
+                // Only render pixels if height is higher than previously rendered terrain at current x position
+                if (heightonscreen < max_height) {
+                    for (int y = heightonscreen; y < max_height; y++) {
+                        if (y >= 0) {
+                            framebuffer[(SCREEN_WIDTH * y) + i] = (uint8_t)colormap[mapoffset];
+                        }
+                    }
+                    max_height = heightonscreen;
+                }
+            }
+
+        }
 
         framebuffer = swapbuffers();
 
